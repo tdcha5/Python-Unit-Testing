a3356965a881cb3070098a51c63f579666eeb100
Tori <toridchambers@gmail.com>
2020-05-31 12:30:29 -0400

    Adding unit test cases for happy path and an HTML reporter for each test for better readability of test cases

--- calc_test/test_calc.py
+++ calc_test/test_calc.py
@@ -1,12 +1,31 @@
 import unittest
 import calc 
+import HtmlTestRunner
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        result = calc.add(10,5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.calculate("Add",10,5), 15)
         
-      ### Add a test case for each operator so that I can run it as I make changes to calc.py
+        ## Edge cases for add functions
+        self.assertEqual(calc.calculate("Add",-1, 1), -0)
+        self.assertEqual(calc.calculate("Add",-1, -1), -2)
+        
+    def test_subtract(self):
+        self.assertEqual(calc.calculate("Subtract",10,5), 5)
+        self.assertEqual(calc.calculate("Subtract",-1,1), -2)
+        self.assertEqual(calc.calculate("Subtract",-1,-1), 0)
+        
+    def test_multiply(self):
+        self.assertEqual(calc.calculate("Multiply",10,5),50)
+        self.assertEqual(calc.calculate("Multiply",-1,1), -1)
+        self.assertEqual(calc.calculate("Multiply",-1,-1), 1)
+        
+    def test_divide(self):
+        self.assertEqual(calc.calculate("Divide",10,5), 2)
+        self.assertEqual(calc.calculate("Divide",-1,1), -1)
+        self.assertEqual(calc.calculate("Divide",5,2), 2.5)
+        
         
 if __name__ == '__main__':
-    unittest.main()
+    ##unittest.main()
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:/Users/torid/Documents/projects/calc_test/reports'))
